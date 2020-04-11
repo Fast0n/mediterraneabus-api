a7a2ace1845601b15a40ca5e3dc71d6f1a2ff19d
Massimiliano Montaleone <ThePlayerGame97@gmail.com>
2020-04-11 14:48:49 +0200

    fix multiple field in /list

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cheerio = require("cheerio");
 const request = require("request");
-const sortJsonArray = require('sort-json-array');
+const sortJsonArray = require("sort-json-array");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.type("application/json");
   // check fields
 
@@ -15,21 +15,21 @@ app.get("/", function (req, res) {
       json: true
     };
 
-    request(options, function (error, response, body) {
+    request(options, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         const $ = cheerio.load(body);
 
         var array = [];
 
         var results = $("body");
-        results.each(function (i, result) {
+        results.each(function(i, result) {
           // take a version
           $(result)
             .find("div.contBox")
             .find("tbody")
             .find('select[name="percorso_linea"]')
             .find("option")
-            .each(function (index, element) {
+            .each(function(index, element) {
               array = array.concat([$(element).text()]);
             });
 
@@ -37,11 +37,15 @@ app.get("/", function (req, res) {
           data_store["list"] = {};
           data_store["list"]["routes"] = [];
 
-          for (var j = 2; j < array.length; j++)
-            data_store["list"]["routes"][j - 2] = array[j].replace(
-              "&apos;",
-              "'"
-            );
+          for (var j = 2; j < array.length; j++) {
+            var value = array[j].replace("&apos;", "'");
+            var value2 = array[j - 1].replace("&apos;", "'");
+            if (value != value2) data_store["list"]["routes"][j - 2] = value;
+          }
+
+          data_store["list"]["routes"] = data_store["list"]["routes"].filter(
+            () => true
+          );
 
           res.send(data_store);
         });
@@ -55,7 +59,8 @@ app.get("/", function (req, res) {
     var warning = {
       STATUS: "1",
       CODE: "200",
-      MESSAGE: "For help, go here -> https://github.com/Fast0n/mediterraneabus-api",
+      MESSAGE:
+        "For help, go here -> https://github.com/Fast0n/mediterraneabus-api",
       DATA: [],
       TOTALS: []
     };
@@ -102,7 +107,7 @@ app.get("/", function (req, res) {
       form: JSONformData
     };
 
-    request(options, function (error, response, body) {
+    request(options, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         var html = body
           .replace(/<td><strong>Corse/g, "<td><titolo>Corse")
@@ -121,7 +126,7 @@ app.get("/", function (req, res) {
         var orari = [];
         var var_b = 0;
         var var_c = 0;
-        results.each(function (i, result) {
+        results.each(function(i, result) {
           var array = [];
           var array1 = [];
           var array2 = [];
@@ -142,7 +147,7 @@ app.get("/", function (req, res) {
           $(result)
             .find("ts")
             .find("strong")
-            .each(function (index, element) {
+            .each(function(index, element) {
               array = array.concat([$(element).text()]);
             });
           if (array != "") {
@@ -154,11 +159,10 @@ app.get("/", function (req, res) {
           $(result)
             .find("tr")
             .children("ts")
-            .each(function (index, element) {
+            .each(function(index, element) {
               var time = $(element)
                 .text()
                 .split("\n\t\t\t\t\t\t\t");
-
 
               // regex Pattern
               var rePattern = new RegExp("([0-9]+.)");
@@ -173,8 +177,11 @@ app.get("/", function (req, res) {
                     .replace("Apri/stampa", "");
 
                   if (time[x].split(":")[0].length == 1)
-                    time[x] = "0" + time[x].split(":")[0] + ":" + time[x].split(":")[1];
-                  else time[x] = time[x].split(":")[0] + ":" + time[x].split(":")[1];
+                    time[x] =
+                      "0" + time[x].split(":")[0] + ":" + time[x].split(":")[1];
+                  else
+                    time[x] =
+                      time[x].split(":")[0] + ":" + time[x].split(":")[1];
                 } else array2[x] = "";
 
                 if (array2[x] != "") array2[x] = time[x].replace("\t", "");
@@ -193,54 +200,52 @@ app.get("/", function (req, res) {
           }
         });
 
-
-
         for (var x = 0; x < var_a.length; x++) {
           data_store[x] = [];
 
           for (var counter = 0; counter < keys[x].length; counter++) {
             data_store[x]["title"] = var_a[x];
             data_store[x][keys[x][counter]] = orari[x][counter];
-
           }
         }
 
-        var global_counter = 0
+        var global_counter = 0;
 
         for (var y = 0; y < Object.keys(data_store).length; y++) {
           try {
-
             for (
-              var z = 0; z < data_store[y][req.query.percorso_linea].length; z++
+              var z = 0;
+              z < data_store[y][req.query.percorso_linea].length;
+              z++
             ) {
               if (
                 data_store[y][req.query.percorso_linea][z] != undefined ||
                 data_store[y][req.query.percorso_linea1][z] != undefined
               ) {
-
-
-                json_store[global_counter] = {}
+                json_store[global_counter] = {};
 
                 for (var counter = -1; counter < z; counter++) {
-                  if (data_store[y][req.query.percorso_linea1][counter + 1] > data_store[y][req.query.percorso_linea][counter + 1]) {
-                    json_store[global_counter]['a'] = data_store[y]["title"]
-                    json_store[global_counter]['b'] = data_store[y][req.query.percorso_linea][counter + 1]
-                    json_store[global_counter]['c'] = data_store[y][req.query.percorso_linea1][counter + 1]
+                  if (
+                    data_store[y][req.query.percorso_linea1][counter + 1] >
+                    data_store[y][req.query.percorso_linea][counter + 1]
+                  ) {
+                    json_store[global_counter]["a"] = data_store[y]["title"];
+                    json_store[global_counter]["b"] =
+                      data_store[y][req.query.percorso_linea][counter + 1];
+                    json_store[global_counter]["c"] =
+                      data_store[y][req.query.percorso_linea1][counter + 1];
                   }
                 }
-                global_counter++
+                global_counter++;
               }
             }
           } catch (err) {}
         }
-        if (result == false)
-          res.send(sortJsonArray(json_store, 'a'));
-        else
-          res.send(sortJsonArray(json_store, 'b'));
-
+        if (result == false) res.send(sortJsonArray(json_store, "a"));
+        else res.send(sortJsonArray(json_store, "b"));
       }
     });
   }
 });
 
-const server = app.listen(process.env.PORT || 1331, function () {});
+const server = app.listen(process.env.PORT || 1331, function() {});
